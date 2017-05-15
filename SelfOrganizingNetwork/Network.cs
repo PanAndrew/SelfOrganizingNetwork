ff28d9669891d593ae2019260ea176814ed8bd02
Piotr Biernacki <203855@edu.p.lodz.pl>
2017-05-15 09:21:13 +0200

    Creating of Network connections

---
+++ SelfOrganizingNetwork/Network.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfOrganizingNetwork
+{
+    public class Network
+    {
+        private double maxX;
+        private double minX;
+        private double maxY;
+        private double minY;
+
+        private int width;
+        private int height;
+
+        public List<List<Neuron>> NetworkOfNeurons { get; set; }
+
+        public Network(int width, int height, List<ObservablePoint> observableList)
+        {
+            NetworkOfNeurons = new List<List<Neuron>>();
+            CountMaxsAndMinsCoordinates(observableList);
+
+            this.width = width;
+            this.height = height;
+
+            for (int i = 0; i < width; i++)
+            {
+                NetworkOfNeurons.Add(new List<Neuron>());
+            }
+
+            FillNetwork();
+
+            SetNeighborhood();
+
+        }
+
+        private void CountMaxsAndMinsCoordinates(List<ObservablePoint> observableList)
+        {
+            maxX = observableList.First().X;
+            minX = observableList.First().X;
+            maxY = observableList.First().Y;
+            maxY = observableList.First().Y;
+
+            foreach (ObservablePoint obsPoint in observableList)
+            {
+                if (obsPoint.X > maxX) maxX = obsPoint.X;
+                if (obsPoint.X < minX) minX = obsPoint.X;
+                if (obsPoint.Y > maxY) maxY = obsPoint.Y;
+                if (obsPoint.Y < minY) minY = obsPoint.Y;
+            }
+        }
+
+        private double ArrangementOfX(int width)
+        {
+            return (maxX - minX) / width;
+        }
+
+        private double ArrangementOfY(int height)
+        {
+            return (maxY - minY) / height;
+        }
+
+        private void FillNetwork()
+        {
+            double portionOfX = 0;
+            double portionOfY = maxY;
+
+            for (int i = 0; i < width; i++)
+            {
+                portionOfX += ArrangementOfX(width);
+                portionOfY = maxY;
+
+                for (int j = 0; j < height; j++)
+                {
+                    portionOfY -= ArrangementOfY(height);
+
+                    NetworkOfNeurons[i].Add(new Neuron(portionOfX,portionOfY));
+                }
+            }
+        }
+
+        private void SetNeighborhood()
+        {
+            for (int i = 0; i < height - 1; i++)
+            {
+                for (int j = 0; j < width - 1; j++)
+                {
+                    NetworkOfNeurons[i][j].AddNeighbour(NetworkOfNeurons[i][j + 1]);
+                    NetworkOfNeurons[i][j + 1].AddNeighbour(NetworkOfNeurons[i][j]);
+                }
+            }
+
+            for (int i = 0; i < width - 1; i++)
+            {
+                for (int j = 0; j < height - 1; j++)
+                {
+                    NetworkOfNeurons[i][j].AddNeighbour(NetworkOfNeurons[i][j + 1]);
+                    NetworkOfNeurons[i][j + 1].AddNeighbour(NetworkOfNeurons[i][j]);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ SelfOrganizingNetwork/Neuron.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfOrganizingNetwork
+{
+    public class Neuron
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        private List<Neuron> neighbours;
+
+        public double Tiredness { get; set; }
+
+        public Neuron(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+
+            neighbours = new List<Neuron>();
+        }
+
+        public Neuron(ObservablePoint obsPoint)
+        {
+            this.X = obsPoint.X;
+            this.Y = obsPoint.Y;
+
+            neighbours = new List<Neuron>();
+        }
+
+        public void AddNeighbour(Neuron neighbour)
+        {
+            neighbours.Add(neighbour);
+        }
+
+    }
+}
